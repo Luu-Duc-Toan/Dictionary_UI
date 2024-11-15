0fabe0c86ea4fc5dd5fbfcaac0089e9ad98c80f9
Luu-Duc-Toan <23120095@student.hcmus.edu.vn>
2024-11-15 11:24:48 +0700

    Fix bug in get input key

--- Dictionary_UI/Main.cpp
+++ Dictionary_UI/Main.cpp
@@ -1,6 +1,7 @@
 #include "Header.h"
 Color toolbarColor = { 29, 42, 87, 255 };
-Color searchBarColor = { 15, 23, 59, 255 }; 
+Color searchBarColor = { 15, 23, 59, 255 };
+
 int main() {
 	int screenWidth = 900;
 	int screenHeight = 900;
@@ -9,7 +10,7 @@ int main() {
 	float pageMarginTop = 10;
 	float margin = 50;
 	int mode = 0; // 0: search, 1: insert, 2: delete, 3: help, 4: contact
-	 
+
 	//for icons
 	int iconSize = 50;
 	float iconPosX = pageMarginLeft;  // X position
@@ -51,7 +52,7 @@ int main() {
 	int suggestionMargin = 3;
 	int suggestionBoxWidth = searchBarWidth;
 	int suggestionBoxHeight = 50;
-	vector<string> suggestions = {"got", "gotten", "Hello", "Gay"};
+	vector<string> suggestions = { "got", "gotten", "Hello", "Gay" };
 
 	//Result
 	int resultMargin = 20;
@@ -62,35 +63,51 @@ int main() {
 	int keySize = 80;
 	int meaningSize = keySize / 2;
 	string key = "get";
-	vector<string> meanings = {"nhan", "hieu", "biet"};
+	vector<string> meanings = { "nhan", "hieu", "biet" };
 
 	while (true) {
-		//ctrl V
-		if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_V)) {
-			const char* clipboardText = GetClipboardText();  
-			if (clipboardText != NULL) {
-				key += clipboardText;
+		if (IsKeyDown(KEY_LEFT_CONTROL)) {
+			//ctrl V 
+			if (IsKeyPressed(KEY_V)) {
+				const char* clipboardText = GetClipboardText();
+				if (clipboardText != NULL) {
+					key += clipboardText;
+				}
+			}
+			//ctrl S
+			else if (IsKeyPressed(KEY_S)) {
+				mode = 0;
+			}
+			//ctrl I
+			else if (IsKeyPressed(KEY_I)) {
+				mode = 1;
+			}
+			//ctrl D
+			else if (IsKeyPressed(KEY_D)) {
+				mode = 2;
+			}
+			//ctrl H
+			else if (IsKeyPressed(KEY_H)) {
+				//Draw Help Page
+			}
+			//ctrl C
+			else if (IsKeyPressed(KEY_C)) {
+				//Draw Contact Page
 			}
 		}
-		//ctrl S
-		if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_S)) {
-			mode = 0;
+
+
+
+		int input = GetKeyPressed();
+		if (input != 0) cout << input << endl;
+		if (input == KEY_BACKSPACE) {
+			if (key.size() > 0) key.pop_back();
 		}
-		//ctrl I
-		if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_I)) {
-			mode = 1;
+		else if (input >= 'A' && input <= 'Z') {
+			meanings.clear();
+			key += input + 32;
+			cout << key << endl;
 		}
-		//ctrl D
-		if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_D)) {
-			mode = 2;
-		}
-		
-		
-		char input = GetKeyPressed();
-		if(input == KEY_BACKSPACE)
-			if(key.size() > 0) key.pop_back();
-		else if(input < 'Z' && input > 'A')
-			key += input;
 		else if (input == KEY_ENTER) {
 			//Search
 			if (mode == 0) {
@@ -109,14 +126,6 @@ int main() {
 			}
 			suggestions.clear();
 		}
-		//Move cursor
-		/*else if (input == KEY_DOWN) {
-			mode = (mode + 1) % 5;
-		}
-		else if (input == KEY_RIGHT) {
-			mode = (mode + 1) % 5;
-		}*/
-		
 
 		//Draw
 		BeginDrawing();
@@ -140,14 +149,14 @@ int main() {
 		}
 		DrawText(key.c_str(), searchBarBoxPos.x + 10, searchBarBoxPos.y + (searchBarHeight - searchBarFontSize) / 2, searchBarFontSize, BLACK);
 		int textWidth = MeasureText(key.c_str(), searchBarFontSize);
-		if(showCursor) DrawText("|", searchCursorX + textWidth, searchCursorY, 50, BLACK);
-		
+		if (showCursor) DrawText("|", searchCursorX + textWidth, searchCursorY, 50, BLACK);
+
 		//Draw Suggestions
 		int i = 0;
 		int suggestionPosY = searchBarBoxPos.y + searchBarHeight + suggestionMargin + (suggestionBoxHeight - suggestionFontSize) / 2;
 		int suggestionBoxPosY = searchBarBoxPos.y + searchBarHeight + suggestionMargin;
-		for (string &suggestion : suggestions) {
-			if(i == hightlightSuggestion) 
+		for (string& suggestion : suggestions) {
+			if (i == hightlightSuggestion)
 				DrawRectangle(searchBarBoxPos.x, suggestionBoxPosY, suggestionBoxWidth, suggestionBoxHeight, GRAY);
 			else DrawRectangle(searchBarBoxPos.x, suggestionBoxPosY, suggestionBoxWidth, suggestionBoxHeight, WHITE);
 			DrawText(suggestion.c_str(), searchBarBoxPos.x + 10, suggestionPosY, suggestionFontSize, BLACK);
@@ -155,21 +164,20 @@ int main() {
 			suggestionBoxPosY += suggestionBoxHeight + suggestionMargin;
 			i++;
 		}
-		
 		//Draw Result
 		int resultPosX = resultStartPosX;
 		int resultPosY = resultStartPosY;
 		DrawText(key.c_str(), resultPosX, resultPosY, keySize, BLACK);
 		resultPosX += resultMargin;
 		resultPosY += keySize;
-		for(string &meaning : meanings) {
+		for (string& meaning : meanings) {
 			resultPosY += meaningSize + resultMargin;
 			DrawText(meaning.c_str(), resultPosX, resultPosY, meaningSize, BLACK);
 		}
-		
+
 		EndDrawing();
 	}
-	
-	for(auto &icon : icons) 
+
+	for (auto& icon : icons)
 		UnloadTexture(icon);
 }
